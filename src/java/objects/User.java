e8658fcea2bb6dd85d57d392eb2f6ab5dea393d2
ASUS <ASUS@192.168.179.216>
2015-03-11 15:14:16 +0400

    Разметка главной страницы и объект User

---
+++ src/java/objects/User.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package objects;
+
+/**
+ *
+ * @author ASUS
+ */
+public class User {
+   private int user_id;
+   private String name;
+   private String surname;
+   private String middlename;
+   private String mail;
+   private String login;
+   private String password;
+   private int is_admin;
+
+    /**
+     * @return the user_id
+     */
+    public int getUser_id() {
+        return user_id;
+    }
+
+    /**
+     * @param user_id the user_id to set
+     */
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * @param surname the surname to set
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * @return the middlename
+     */
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    /**
+     * @param middlename the middlename to set
+     */
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    /**
+     * @return the mail
+     */
+    public String getMail() {
+        return mail;
+    }
+
+    /**
+     * @param mail the mail to set
+     */
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the is_admin
+     */
+    public int getIs_admin() {
+        return is_admin;
+    }
+
+    /**
+     * @param is_admin the is_admin to set
+     */
+    public void setIs_admin(int is_admin) {
+        this.is_admin = is_admin;
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -33,4 +33,7 @@
             out.print("<br>");
         }
     %>
+    <a href="/youraction" data-paypal-button="true">
+  <img src="//www.paypalobjects.com/en_US/i/btn/btn_xpressCheckout.gif" alt="Check out with PayPal" />
+</a>
 </html>
